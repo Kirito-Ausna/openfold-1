bce3ca01ab979d2c29a03ad2f5a47e7ffe7e2819
huangyufei <huangyufei@zju.edu.cn>
2022-02-14 06:23:25 +0000

    change to bfp16 training

--- train_manifold.sh
+++ train_manifold.sh
@@ -1,14 +1,16 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PDB_Dir="/usr/commondata/local_public/rep/train/pdb/"
+train_dir="/usr/commondata/local_public/rep/train/pdb/"
+# train_dir="/usr/commondata/public/CASP14_Datasets/datasets/pdb_mmcif/mmcif_files/"
 alignment_dir="/usr/commondata/local_public/rep/train/a3m/"
+# alignment_dir="/root/AlphaFold-Pytorch/openfold/dataset_generate/alignment_dir/"
 template_mmcif_dir="/usr/commondata/public/CASP14_Datasets/datasets/pdb_mmcif/mmcif_files/"
 output_dir="/root/AlphaFold-Pytorch/openfold/output_dir"
-python3 train_openfold.py $PDB_Dir $alignment_dir $template_mmcif_dir $output_dir\
+python3 train_openfold.py $train_dir $alignment_dir $template_mmcif_dir $output_dir\
     2021-10-10\
-    --precision 16\
+    --precision bf16\
     --gpus 4\
     --replace_sampler_ddp True\
-    --seed 42\
+    --seed 36\
     --deepspeed_config_path ./deepspeed_config.json
     # --resume_from_ckpt ckpt_dir/
     # in multi-gpu settings, the seed must be specified
